23fc3c9589fe2cc0050a83ec7fced13103c91acf
CodreanuAlexandru <70651712+CodreanuAlexandru@users.noreply.github.com>
2020-09-16 10:34:29 +0300

    Delete Problema 5.py

--- Problema 5.py
+++
@@ -1,6 +0,0 @@
-
-print ("x    |y    |x and y|x or y|not x|not y|")
-print ("True |False|False  |True  |False|True |")
-print ("False|True |False  |True  |True |False|")
-print ("False|False|False  |False |True |True |")
-print ("True |True |True   |True  |False|False|")
